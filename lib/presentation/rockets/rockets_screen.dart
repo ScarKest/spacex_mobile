5ad7174c9bb803ecde6da7c20f922aff9f92c70f
Kestler Barrios <luiseduardo3213@gmail.com>
2024-09-26 00:56:53 -0600

    Show rockets in screen

--- lib/presentation/rockets/rockets_screen.dart
+++ lib/presentation/rockets/rockets_screen.dart
@@ -2,9 +2,9 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:spacex_mobile/data/models/rocket_model.dart';
+import 'package:spacex_mobile/injection.dart';
 import 'package:spacex_mobile/presentation/rockets/cubit/rocket_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:spacex_mobile/injection.dart';
 
 class RocketsScreen extends StatefulWidget {
   const RocketsScreen({super.key});
@@ -14,44 +14,54 @@ class RocketsScreen extends StatefulWidget {
 }
 
 class _RocketsScreenState extends State<RocketsScreen> {
+  late RocketCubit rocketCubit;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    rocketCubit.close();
+    super.dispose();
+  }
+
+  void _getRockets(BuildContext contextBloc) {
+    final rocketCubit = contextBloc.read<RocketCubit>();
+    rocketCubit.getRocket();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Rockets Screen'),
+      appBar: AppBar(
+        title: const Text('Rockets Screen'),
+      ),
+      body: BlocProvider(
+        create: (_) => sl<RocketCubit>(),
+        child: BlocBuilder<RocketCubit, RocketState>(
+          builder: (context, state) {
+            _getRockets(context);
+            if (state is RocketSuccess) {
+              // log(state.rocket.toString());
+              RocketModel rocketModel = state.rocket;
+              return ListView.builder(
+                itemCount: rocketModel.data.rockets.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(rocketModel.data.rockets[index].name),
+                  subtitle: Text(rocketModel.data.rockets[index].id),
+                ),
+              );
+            } else if (state is RocketLoading) {
+              return const Text('Loading');
+            } else if (state is RocketInitial) {
+              return const Text('Iniasdasdasdasda');
+            }
+
+            return const Text('No lo');
+          },
         ),
-        body: BlocProvider(
-          create: (_) => sl<RocketCubit>(),
-          child: BlocConsumer<RocketCubit, RocketState>(
-            listener: (context, state) {
-              if (state is RocketLoading) {
-                log('Loading');
-              } else if (state is RocketError) {
-                log('error');
-              } else if (state is RocketSuccess) {
-                log('SUCCESS');
-              }
-            },
-            builder: (context, state) {
-              final rocketCubit = context.read<RocketCubit>();
-              rocketCubit.getRocket();
-              if (state is RocketSuccess) {
-                RocketModel rocketModel = state.rocket;
-                return ListView(
-                  children: [
-                    Text(rocketModel.data.rockets.first.name),
-                  ],
-                );
-              } else if (state is RocketError) {
-                return Text('Error ${state.error}');
-              } else if (state is RocketInitial) {
-                return const Text('InitiaKGIOIUl');
-              } else if (state is RocketLoading) {
-                return const Text('Loading');
-              }
-              return const Text('No lo');
-            },
-          ),
-        ));
+      ),
+    );
   }
 }
